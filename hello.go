7f0d2ea4bf42d1770a7c18ee3e890a8be6ddb02e
Mahdi Ghaharian <m.ghaharian@gmail.com>
2019-12-09 16:04:40 +0330

    added sieve implementation

--- hello.go
+++ hello.go
@@ -1,11 +1,21 @@
 package main
 
-import "fmt"
-import "runtime"
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/mghhrn/go-playground/prime-sieve"
+	MyStringUtil "github.com/mghhrn/go-stringutil"
+)
+
 
 func main() {
 	fmt.Printf("hello, world\n")
 	fmt.Printf("number of cpu cores = ")
 	fmt.Printf("%d\n", runtime.NumCPU())
+
+	fmt.Println(MyStringUtil.Reverse("!oG ,olleH"))
+	prime_sieve.Sieve();
+
 }
 
